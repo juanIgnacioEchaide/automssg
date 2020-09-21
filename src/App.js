4586dfe5a18adf527b08d724104ffb779770e378
Juan Ignacio Echaide <juan.ignacio.echaide@accenture.com>
2020-09-21 13:26:31 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import {Form, Button} from 'react-bootstrap';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 import './App.css';
 
 function App() {
+
+  const[datosCliente, setDatosCliente]=useState({
+    nombre:'',
+    dia:'',
+    hora:''
+  });
+
+  const handleChange=(event)=>{
+    setDatosCliente({
+      ...datosCliente,
+     [ event.target.name]:event.target.value
+    })
+    console.log(datosCliente);
+  }
+
+  const[parrafo, setParrafo]=useState({
+    parrafo:''
+  });
+
+  const funcion=(event)=>{
+    setParrafo({
+      
+      parrafo:event.target.innerText
+    });
+    console.log(parrafo)
+  }
+
+  const[value,setValue]=useState('');
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <Form>
+      <Form.Group>
+        <Form.Label>Cliente</Form.Label>
+        <Form.Control  name="nombre" type="text" placeholder="Enter email" onChange={handleChange}/>
+        <Form.Label>Día Turno</Form.Label>
+        <Form.Control  name="dia" type="text"  placeholder="Enter email" onChange={handleChange}/>
+        <Form.Label>Hora Turno</Form.Label>
+        <Form.Control name="hora" type="text"  placeholder="Enter email" onChange={handleChange}/> 
+      </Form.Group>
+    </Form>
+
+      <div id="parrafo" name="parrafo" value={parrafo} onClick={funcion}>
+        <p > 
+          Estimado {datosCliente.nombre}:
+          Tiene agendado un turno para el día {datosCliente.dia} a las {datosCliente.hora} hs,
+           en la Sucursal ICBC-Flores. ¿Es correcto?
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <CopyToClipboard text={parrafo} >
+        <Button variant="Success" type="success" >
+          Copy
+        </Button>
+      </CopyToClipboard>
+      </div> 
     </div>
   );
 }
